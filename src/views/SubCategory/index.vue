f1cf09856ca4bf042502b5bc0480fd801321ed75
arsoin <hamehamepa@outlook.com>
2024-12-04 16:15:27 +0900

    列表无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -33,6 +33,39 @@ const getGoodList = async()=>{
 onMounted(()=>{
   getGoodList()
 })
+const disabled = ref(false)
+//tab 切换
+const tabChange = ()=>{
+  reqDate.value.page = 1
+  console.log('tab切换了',reqDate.value.sortField);
+  getGoodList()
+  disabled = false
+}
+
+//加载更多
+const load = async()=>{
+  // console.log('加载更多数据')
+  //获取下一页数据
+  reqDate.value.page++
+  //新老数据拼接
+  //新数据
+  const res = await getSubCategoryAPI(reqDate.value)
+  //拼接
+  goodList.value = [...goodList.value,...res.result.items]
+  /*
+    ... 是 ES6 引入的展开语法，用于将数组中的元素逐个取出。
+  [...goodList.value, ...res.result.items]：
+  ...goodList.value：将 goodList.value 数组中的所有元素展开。
+  ...res.result.items：将 res.result.items 数组中的所有元素展开。
+  两个展开结果被拼接到一个新的数组中。
+  */
+    //加载完毕，停止监听
+  if(res.result.items.length === 0){
+    disabled.value = true
+  }
+}
+
+
 
 
 </script>
@@ -49,12 +82,13 @@ onMounted(()=>{
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <!-- 这里v-model和name 双向绑定，这边name的数据可以直接传到这里面去-->
+      <el-tabs v-model="reqDate.sortField" @tab-change="tabChange" :infinite-scroll-disabled="disabled">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load">
          <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id"/>
       </div>
